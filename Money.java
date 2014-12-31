5136fd67b4b08bf3c0cd506cd7389160a9a37f0e
Majid Ali <m_jatoi73@yahoo.com>
2014-12-31 16:31:12 +0500

    Example12

---
+++ Money.java
@@ -0,0 +1,11 @@
+package sibisoft.code;
+
+public class Money {
+
+	protected int amount;
+	
+	public boolean equals(Object object) {
+		Money money= (Money) object;
+		return amount == money.amount;
+	}
+}
