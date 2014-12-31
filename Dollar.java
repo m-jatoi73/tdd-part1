c9db40d3c36a62c7804e0fb08fe62bd575c02468
Majid Ali <m_jatoi73@yahoo.com>
2014-12-31 11:59:16 +0500

    Example4

--- Dollar.java
+++ Dollar.java
@@ -3,12 +3,13 @@ package sibisoft.code;
 public class Dollar {
 
 	int amount;
-	
+
 	Dollar(int amount) {
 		this.amount= amount;
 	}
-	
-	void times(int multiplier) {
+
+	Dollar times(int multiplier) {
 		amount *= multiplier;
+		return null;
 	}
 }
