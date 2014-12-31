a67f056f5e0da6fb53cb5f3e0628339136a455f0
Majid Ali <m_jatoi73@yahoo.com>
2014-12-31 14:08:15 +0500

    Example10

--- TestJunit.java
+++ TestJunit.java
@@ -9,9 +9,7 @@ public class TestJunit {
 	@Test
    public void testMultiplication() {
 		Dollar five= new Dollar(5);
-		Dollar product= five.times(2);
-		assertEquals(new Dollar(10), product);
-		product= five.times(3);
-		assertEquals(15, product.amount);
+		assertEquals(new Dollar(10), five.times(2));
+		assertEquals(new Dollar(15), five.times(3));
    }
 }
