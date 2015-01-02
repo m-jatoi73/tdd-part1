c292f94944cc7cabe975402120a2cbfe391cdce3
Majid Ali <m_jatoi73@yahoo.com>
2015-01-02 14:56:41 +0500

    Example22

--- TestJunit.java
+++ TestJunit.java
@@ -6,12 +6,8 @@ import org.junit.Test;
 
 public class TestJunit {
 	@Test
-	public void testMixedAddition() {
-		Expression fiveBucks= Money.dollar(5);
-		Expression tenFrancs= Money.franc(10);
-		Bank bank= new Bank();
-		bank.addRate("CHF", "USD", 2);
-		Money result= bank.reduce(tenFrancs.plus(fiveBucks), "USD");
-		assertEquals(Money.dollar(10), result);
+	public void testPlusSameCurrencyReturnsMoney() {
+		Expression sum= Money.dollar(1).plus(Money.dollar(1));
+		assertTrue(sum instanceof Money);
 	}
 }
